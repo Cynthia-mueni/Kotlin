a1757a884703ce1fdb79a81493b8a342048bf48c
Cynthia-mueni <nthenyamuenicynthia@gmail.com>
2024-03-18 22:26:59 +0300

    calcutation

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main() {
+   //write a program the user to enter two numbers and prints their sum,diffrence, product and quotient
+    var a = 20
+    var b = 10
+    println(a+b)
+    println(a*b)
+    println( a/b)
+    println(a%b)
+//write a program that calculate the area of a rectangle. the user should be prompt to enter the length and  width of the rectangle.
+var length  = 20
+    var width = 30
+    var area = length * width
+    println(area)
+//    write a program that convert the tempereture from celcius to fahrenheit.the user should be prompte to user the tempeteture in celcius
+    var fullName = "Cynthia Mueni"
+    var age = 20
+    var phone = "0702553057"
+    var weight = 85.5F
+    var citizen = "not Kenyan"
+    println(fullName)
+    println(age)
+    println(weight)
+    println(phone)
+    println(citizen)
+
+    var name = "Val"
+    var age = 20
+    var color = "blue"
+    var result ="My name is $name am $age years old my favourite $color"
+    print(result)
+
+}
+
